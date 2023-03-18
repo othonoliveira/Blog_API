56f5fa09ec85bfa2fe425301681581162e9e74ee
Othon Oliveira <othonoliveira08@gmail.com>
2023-03-17 23:40:20 -0300

    Requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING
+  },
+  {
+    timestamps: false,
+    tableName: 'users',
+    underscored: true,
+  }
+  );
+  
+  User.associate = (models) => {
+    User.hasMany(models.BlogPosts, {
+      foreignKey: 'userId',
+      as: 'blog_posts'
+    })
+  }
+  
+  return User;
+};
